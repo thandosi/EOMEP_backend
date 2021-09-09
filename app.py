103919b08559b6c3d9b99a25e71458d02cc4d740
administrator <siyanjomeni@gmail.com>
2021-09-09 11:32:33 +0200

    first commit

--- app.py
+++ app.py
@@ -262,7 +262,30 @@ def get_users():
     response['data'] = tuple(accumulator)
     return jsonify(response)
 
+
 # get flights
+@app.route('/get-user', methods='PATCH')
+@cross_origin()
+def user_login():
+    response = {}
+    if request.method == 'PATCH':
+        try:
+            username = request.json['username']
+            password = request.json['password']
+
+            with sqlite3.connect('reservation.db') as conn:
+                cursor = conn.cursor()
+                cursor.row_factory = sqlite3.Row
+                cursor.execute('SELECT * FROM passengers WHERE username=? AND password=?', (username, password))
+                post = cursor.fetchone()
+                response['status_code'] = 200
+                response['message'] = 'Account' + str(username) + 'collected'
+                response['data'] = post
+                return response
+        except ValueError:
+            response['message'] = 'Please enter correct username or password !'
+            response['status_code'] = 400
+            return response
 
 
 @app.route('/get-flight/', methods=["GET"])
